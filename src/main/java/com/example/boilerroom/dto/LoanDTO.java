28531ea3d688239a1c7c75cdc883d658e29b3062
BobbyBrogrammer <robin.lindholm111@gmail.com>
2026-03-29 22:43:07 +0200

    (V3-Upg4) Added: LoanDTO, LoanRepository, LoanService and Loan Controller
    ..to handle loan CRUD, also added validation to prevent a book from beign loaned twice

---
+++ src/main/java/com/example/boilerroom/dto/LoanDTO.java
@@ -0,0 +1,25 @@
+package com.example.boilerroom.dto;
+
+import java.time.LocalDate;
+
+public class LoanDTO {
+    private Long id;
+    private Long bookId;
+    private String bookTitle;
+    private LocalDate loanDate;
+    private LocalDate returnDate;
+
+    // Getters
+    public Long getId() {return id;}
+    public Long getBookId() {return bookId;}
+    public String getBookTitle() {return bookTitle;}
+    public LocalDate getLoanDate() {return loanDate;}
+    public LocalDate getReturnDate() {return returnDate;}
+
+    // Setters
+    public void setId(Long id) {this.id = id;}
+    public void setBookId(Long bookId) {this.bookId = bookId;}
+    public void setBookTitle(String bookTitle) {this.bookTitle = bookTitle;}
+    public void setLoanDate(LocalDate loanDate) {this.loanDate = loanDate;}
+    public void setReturnDate(LocalDate returnDate) {this.returnDate = returnDate;}
+}
