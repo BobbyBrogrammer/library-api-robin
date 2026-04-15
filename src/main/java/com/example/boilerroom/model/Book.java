a00306e89c06d252dcd35b5c230fee47a2685aa4
BobbyBrogrammer <robin.lindholm111@gmail.com>
2026-04-15 02:31:14 +0200

    (V5-Upg3,4,5)docs: added notes for transactional, locking and scalability

--- src/main/java/com/example/boilerroom/model/Book.java
+++ src/main/java/com/example/boilerroom/model/Book.java
@@ -6,6 +6,10 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version
+    private Long version;
+
     private String title;
 
     @ManyToOne
@@ -23,6 +27,7 @@ public class Book {
     }
     // Getters
     public Long getId() {return id;}
+    public Long getVersion() {return version;}
     public String getTitle() {return title;}
     public Author getAuthor() {return author;}
     public String getIsbn() {return isbn;}
@@ -30,10 +35,9 @@ public class Book {
 
     // Setters
     public void setId(Long id) {this.id = id;}
+    public void setVersion(Long version) {this.version = version;}
     public void setTitle(String title) {this.title = title;}
     public void setAuthor(Author author) {this.author = author;}
     public void setIsbn(String isbn) {this.isbn = isbn;}
     public void setPublishedYear(int publishedYear) {this.publishedYear = publishedYear;}
-
-
 }
